4da1ee0be127d83321e3dfb3a3a42e30b5f2a4cb
uraneptus <uraneptus33@gmail.com>
2025-05-05 18:37:37 +0200

    sorry

--- Xplat/src/main/java/com/mosaicmodding/mosaic_cosmetics/Definitions.java
+++ Xplat/src/main/java/com/mosaicmodding/mosaic_cosmetics/Definitions.java
@@ -13,7 +13,6 @@ public class Definitions {
     );
     public static final Set<Map<List<String>, String>> CONTRIBUTORS = Set.of(
             Map.of(List.of("sullysmod"), "a69fc0f5-028a-4bee-9102-26ff3764017e"), //Sully alt
-            Map.of(List.of("sullysmod", "painters_inc"), "456e2ae6-74b3-474a-9560-92b782bce747"), //Graus
             Map.of(List.of("sullysmod"), "2d173722-de6b-4bb8-b21b-b2843cfe395d"), //Ninni
             Map.of(List.of("sullysmod", "painters_inc"), "7d43a0a0-f228-4382-9d0e-24b96f92ce39"), //Shable
             Map.of(List.of("sullysmod", "painters_inc"), "b2e40db2-5aa8-435a-9c30-8f79efffaba6"), //Angery
@@ -41,20 +40,4 @@ public class Definitions {
             Map.of(List.of("painters_inc"), "7e76e9e2-4080-4845-a025-380422c5afe2") //The Caretaker
 
     );
-
-    /*sullysmod
-    static final Set<UUID> CONTRIBUTORS = Set.of(
-            UUID.fromString("a69fc0f5-028a-4bee-9102-26ff3764017e"), //Sully alt
-            UUID.fromString("456e2ae6-74b3-474a-9560-92b782bce747"), //Graus
-            UUID.fromString("2d173722-de6b-4bb8-b21b-b2843cfe395d"), //Ninni
-            UUID.fromString("7d43a0a0-f228-4382-9d0e-24b96f92ce39"), //Shable
-            UUID.fromString("b2e40db2-5aa8-435a-9c30-8f79efffaba6"), //Angery
-            UUID.fromString("4378df24-8433-4b5c-b865-bf635b003ebb"), //Farcr
-            UUID.fromString("852646ab-620d-48f7-974c-6451100f70f0"), //ibrokemyribcage
-            UUID.fromString("4d659277-fbcc-4fd8-9e80-c8e4c73d10b5"), //sunken past artist
-            UUID.fromString("ce9dd341-b1c2-44d9-a014-71e11d163b01"), //LudoCrypt
-            UUID.fromString("7a422124-11eb-4064-94a7-7741ad45e8f3") //Samir
-    );
-
-     */
 }
